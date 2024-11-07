48b8f8354c5959f95d79269e959aa8475f90952c
Josh Crawford <joshua@sgroup.com.au>
2024-11-08 09:37:41 +1100

    Fix some integrations causing `post_max_size` and `input_max_vars` issues on Craft Cloud

--- src/web/assets/forms/src/js/formie-form.js
+++ src/web/assets/forms/src/js/formie-form.js
@@ -162,8 +162,16 @@ Craft.Formie.EditForm = Garnish.Base.extend({
                 },
 
                 getFormData(options = {}) {
-                    const formElem = this.getFormElement();
-                    const data = new FormData(formElem);
+                    const formElement = this.getFormElement();
+                    const formData = new FormData(formElement);
+                    const data = new FormData();
+
+                    // Filter out empty integration field mapping values, to keep payload size down
+                    for (const [key, value] of formData.entries()) {
+                        if (!(key.startsWith('settings[integrations]') && value === '')) {
+                            data.append(key, value);
+                        }
+                    }
 
                     // Quick-n-easy clone
                     const pageData = clone(this.form.pages);
